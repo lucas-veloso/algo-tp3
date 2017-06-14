1ea3bd76104f7e861ff1c76388861afe26d701c1
gonzadiz <gonzadma@gmail.com>
2017-06-14 19:41:19 -0300

    pruebas de volumen

--- testing.py
+++ testing.py
@@ -9,7 +9,7 @@ def print_test(cadena,bool):
 
 
 def pruebas_grafo():
-	print ("-------- PRUEBAS CREAR GRAFO --------")
+	print("-------- PRUEBAS CREAR GRAFO --------")
 	grafo = Grafo()
 	print_test("El grafo esta vacio",grafo.tamanio_grafo() == 0)
 	print_test("Quitar un vertice en un grafo vacio es false",grafo.quitar_vertice(1) == False)
@@ -37,6 +37,62 @@ def pruebas_grafo():
 	print_test("Eliminar el vertice Aceite",grafo.quitar_vertice("aceite"))
 	print_test("Verificar que huevos no sea adyacente a aceite",grafo.son_adyacentes("huevos","aceite") == False)
 	print_test("El tamanio del grafo es 2",grafo.tamanio_grafo() == 2)
+	print_test("Agregar Leche otra vez es falso ya pertenece al grafo",grafo.agregar_vertice("leche") == False)
+
+def pruebas_volumen():
+	print("-------- PRUEBAS VOLUMEN --------")
+	grafo = Grafo()
+	ok = True
+	print("Prueba agregar 50000 elementos")
+	for i in range(0,50000):
+		ok = grafo.agregar_vertice(i)
+		if not ok:
+			break
+
+	if (not ok):
+		print("Prueba agregar 50000 vertices error")
+
+	print_test("Verificar tamanio del grafo",grafo.tamanio_grafo() == 50000)
+
+	for i in range(0,50000):
+		ok = grafo.vertice_pertenece(i)
+		if not ok:
+			break
+
+	if (not ok):
+		print("ERROR")
+	else:
+		print("Prueba vertices que pertenecen: OK")
+
+	for i in range(0,1000):
+		ok = grafo.agregar_arista(i,i+1)
+		if (not ok):
+			break
+
+	if (ok):
+		print("Prueba agregar 10000 aristas Ok")
+
+	for i in range(0,1000):
+		ok = grafo.son_adyacentes(i,i+1)
+		if (not ok):
+			break
+	if (ok):
+		print("Prueba son adyacentes: Ok")
+
+	for i in range(0,25000):
+		ok = grafo.quitar_vertice(i)
+		if not ok:
+			break
+
+	if (ok):
+		print("Prueba quitar 2500 vertices: OK")
+	else:
+		print("ERROR")
+
+	print_test("Verificar tamanio del grafo",grafo.tamanio_grafo() == 25000)
+
+
 
 '''    MAIN   '''
 pruebas_grafo()
+pruebas_volumen()
