6a0bd1e32e3becac788bfa6678805eae8e43e28a
gonzadiz <gonzadma@gmail.com>
2017-06-14 15:31:56 -0300

    arreglada indentacion

--- grafo.py
+++ grafo.py
@@ -16,7 +16,7 @@ class Grafo(object):
 	def quitar_vertice(self,vertice):
 		'''BOOLEAN ELIMINA UN VERTICE, DEBO SACARLO DE SUS ADYACENTES TAMBIEN'''
 		if (not self.vertice_pertenece(vertice)): #Si no pertenece al grafo no lo puedo quitar
-            return False
+			return False
 		adyacentes = self.obtener_adyacentes(vertice) 
 		for i in adyacentes:
 			self.vertices[i].pop(vertice,None)
@@ -25,51 +25,53 @@ class Grafo(object):
 		return True
 
 	def agregar_arista(self,vertice1,vertice2):
-        '''BOOLEAN AGREGA LA ARISTA FORMADA POR EL VERTICE 1 Y EL VERTICE 2
-        (SE CREA UNA ADYACENCIA ENTRE ELLOS)'''
-        if (not self.vertice_pertenece(vertice1) || not self.vertice_pertenece(vertice2)): #Si no existe algun vertice entonces no puede haber adyacencia
-	        return False
-        if (vertice1 == vertice2):
-            return False
-        if (self.son_adyacentes(vertice1,vertice2)): #Si ya existe la arista no la vuelvo a agregar
-            return False
-        self.vertices[vertice1][vertice2] = True
-        self.vertices[vertice2][vertice1] = True
-        return True
+		'''BOOLEAN AGREGA LA ARISTA FORMADA POR EL VERTICE 1 Y EL VERTICE 2 
+		(SE CREA UNA ADYACENCIA ENTRE ELLOS)'''
+		if (not self.vertice_pertenece(vertice1) or not self.vertice_pertenece(vertice2)): #Si no existe algun vertice entonces no puede haber adyacencia
+			return False
+		if (vertice1 == vertice2):
+			return False
+		if (self.son_adyacentes(vertice1,vertice2)): #Si ya existe la arista no la vuelvo a agregar
+			return False
+		self.vertices[vertice1][vertice2] = True
+		self.vertices[vertice2][vertice1] = True
+		return True
 
 	def quitar_arista (self,vertice1,vertice2):
-         '''BOOLEAN ELIMINA LA ARISTA FORMADA POR ELV ERTICE 1 Y EL VERTICE2
-        (ELIMINA SU ADYACENCIA'''
-        if (vertice1 == vertice2):
-                return False
-        if (not self.son_adyacentes(vertice1,vertice2)): #Si no existe adyacencia no puedo borrar la arista
-                return False 
-        self.vertices[vertice1].pop(vertice2,None)
-        self.vertices[vertice2].pop(vertice1,None)
-        return True
+		'''BOOLEAN ELIMINA LA ARISTA FORMADA POR ELV ERTICE 1 Y EL VERTICE2
+		(ELIMINA SU ADYACENCIA'''
+		if (vertice1 == vertice2):
+			return False
+		if (not self.son_adyacentes(vertice1,vertice2)): #Si no existe adyacencia no puedo borrar la arista
+			return False 
+		self.vertices[vertice1].pop(vertice2,None)
+		self.vertices[vertice2].pop(vertice1,None)
+		return True
+
 	def tamanio_grafo(self):
 		'''DEVUELVE LA CANTIDAD DE VERTICES QUE TIENE EL GRAFO'''
 		return self.cantidadVertices
 
-    def vertice_pertenece(self,vertice):
-        '''BOOLEAN VERIFICA SI UN VERTICE EXISTE EN EL GRAFO'''
-        if (vertice in self.vertices):
-            return True
-        else return False
+	def vertice_pertenece(self,vertice):
+		'''BOOLEAN VERIFICA SI UN VERTICE EXISTE EN EL GRAFO'''
+		if (vertice in self.vertices):
+			return True
+		else:
+			return False
 
 	def son_adyacentes(self,vertice1,vertice2):
 		'''BOOLEAN DEVUELVE TRUE SI DOS VERTICES SON ADYACENTES'''
-	    if (not self.vertice_pertenece(vertice1) || not self.vertice_pertenece(vertice2)): #Si no existe algun vertice entonces no puede haber adyacencia
-	        return False
-		if (vertice1 in self.vertices[vertice2] && vertice2 in self.vertices[vertice1]): #Doble verificacion de adyacencia 
+		if (not self.vertice_pertenece(vertice1) or not self.vertice_pertenece(vertice2)): #Si no existe algun vertice entonces no puede haber adyacencia
+			return False
+		if (vertice1 in self.vertices[vertice2] and vertice2 in self.vertices[vertice1]): #Doble verificacion de adyacencia 
 			return True
 		else:
 			return False
 
 	def obtener_adyacentes(self,vertice):
 		'''DEVUELVE UNA LISTA CON TODOS LOS VERTICES ADYACENTES'''
-        if (not self.vertice_pertenece(vertice)):
-            return None
+		if (not self.vertice_pertenece(vertice)):
+			return None
 		lista_adyacentes = []
 		for adyacente in self.vertices[vertice].keys():
 			lista_adyacentes.append(adyacente)
