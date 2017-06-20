ee5cfdeb952c86599b92f2200a5b5bb54ca5b833
queara <velosolucas6@gmail.com>
2017-06-20 13:25:27 -0700

    optimizando la cola

--- cola.py
+++ cola.py
@@ -1,14 +1,16 @@
+import Queue
+
 class Cola(object):
 	"""
 	Implementacion de cola con los metodos necesarios para utilizar el iterador del grafo
 	"""
 
 	def __init__(self):
-		self.elementos = []
+		self.elementos = Queue.Queue()
 		self.tam = 0
 
 	def encolar(self,elemento):
-		self.elementos.append(elemento)
+		self.elementos.put(elemento)
 		self.tam += 1
 		return True
 
@@ -19,7 +21,7 @@ class Cola(object):
 		"""
 		if self.esta_vacia():
 			return None
-		elem = self.elementos.pop(0)
+		elem = self.elementos.get()
 		self.tam -= 1
 		return elem
 
