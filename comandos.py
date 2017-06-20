c6b1955b6daaa5df37e9fdd676151eb55a28d12c
gonzadiz <gonzadma@gmail.com>
2017-06-19 23:36:01 -0300

    agregadas verificaciones de usuarios falsos, rang cambiado por xrange

--- comandos.py
+++ comandos.py
@@ -13,9 +13,9 @@ def rw (grafo,usuario,guardar_adyacentes):
 	SI <guardar_adyacentes> ES FALSE ENTONCES NO SE GUARDARAN EN EL DICCIONARIO SI ES TRUE SI SE GUARDARAN'''
 
 	random_walk = {}
-	for i in range(0,CANTIDAD_CAMINOS):
+	for i in xrange(0,CANTIDAD_CAMINOS):
 		adyacentes = grafo.obtener_adyacentes(usuario)
-		for j in range(0,LARGO_CAMINO):
+		for j in xrange(0,LARGO_CAMINO):
 			random = randint(0,len(adyacentes)-1)
 			if (adyacentes[random] in random_walk):
 				random_walk[adyacentes[random]] +=1
@@ -31,7 +31,7 @@ def crear_lista_rw(diccionario):
 	''' TRANSFORMA UN DICCIONARIO DE CLAVES Y OCURRENCIAS EN UNA LISTA DE LISTAS "ORDENADA"
 	DE MAYOR A MENOR '''
 	lista_rw = [None] * (max(diccionario.values())+1)
-	for i in range(0,len(lista_rw)):
+	for i in xrange(0,len(lista_rw)):
 		lista_rw[i] = []
 	for clave,ocurrencias in diccionario.iteritems():
 		lista_rw[ocurrencias].append(clave)
@@ -41,9 +41,9 @@ def imprimir_lista_rw(lista,cantidad):
 	''' IMPRIME LA CANTIDAD ESPECIFICADA EN ORDEN DE UNA LISTA DE LISTAS '''
 	contador = 0
 	corte_control = False
-	for i in range(len(lista)-1,0,-1):
+	for i in xrange(len(lista)-1,0,-1):
 		if (lista[i] != []):
-			for j in range(0,len(lista[i])):
+			for j in xrange(0,len(lista[i])):
 				print lista[i][j],
 				contador +=1
 				if (contador == cantidad):
@@ -59,6 +59,9 @@ class Comandos(object):
 	def similares(self,grafo,usuario,cantidad):
 		'''DEVUELVE UNA LISTA CON LOS N USUARIOS MAS SIMILARES AL USUARIO
 		DE MAYOR A MENOR SIMILARIDAD'''
+		if (not grafo.vertice_pertenece(usuario)):
+			print ("El usuario espesificado no pertenece a youtube.")
+			return
 		t_i = time()
 		if (not grafo.vertice_pertenece(usuario)):
 			return None
@@ -71,6 +74,9 @@ class Comandos(object):
 		
 	def recomendar(self,grafo,usuario,cantidad):
 		'''DEVUELVE UNA LISTA DE USUARIOS SIMILARES CON LOS CUALES NO TENGA RELACION'''
+		if (not grafo.vertice_pertenece(usuario)):
+			print ("El usuario espesificado no pertenece a youtube.")
+			return
 		t_i = time()
 		if (not grafo.vertice_pertenece(usuario)):
 			return None
@@ -89,10 +95,10 @@ class Comandos(object):
 		t_i = time()
 		v_centrales = {}
 		vertices = grafo.obtener_identificadores()
-		for i in range(0,CANTIDAD_CAMINOS):
+		for i in xrange(0,CANTIDAD_CAMINOS):
 			vertice_random = random.choice(vertices)
 			adyacentes = grafo.obtener_adyacentes(vertice_random)
-			for j in range(0,LARGO_CAMINO_CENTRALIDAD):
+			for j in xrange(0,LARGO_CAMINO_CENTRALIDAD):
 				p_random = randint(0,len(adyacentes)-1)
 				if (adyacentes[p_random] in v_centrales):
 					v_centrales[adyacentes[p_random]] +=1
@@ -108,19 +114,31 @@ class Comandos(object):
 		print ("Centralidad tardo: ") + str(t_t)
 
 	def camino(self,grafo,origen,destino):
+		if (not grafo.vertice_pertenece(origen) or not grafo.vertice_pertenece(destino)):
+			print ("El usuario espesificado no pertenece a youtube.")
+			return
+		t_i = time()
 		padres = caminos_minimos(grafo,origen,destino)
+		if (padres == None):
+			print("No hay camino entre estos dos usuarios")
+			return
+		t_f = time()
+		print ("Caminos minimos tardo: ") + str(t_f - t_i)
 		camino = []
 		llegada = padres[destino]
 		while llegada != origen :
 			camino.insert(0,llegada)
 			llegada = padres[llegada]
 	
-			print str(origen) + "->",
-			for i in range(0,len(camino)):
-				print str(camino[i]) + "->",
-			print str(destino)
+		print str(origen) + "->",
+		for i in xrange(0,len(camino)):
+			print str(camino[i]) + "->",
+		print str(destino)
 	
 	def distancias(self,grafo,usuario):
+		if (not grafo.vertice_pertenece(usuario)):
+			print ("El usuario espesificado no pertenece a youtube.")
+			return
 		print ("Iniciando BFS...")
 		tiempo_inicial = time()
 		orden = bfs_orden(grafo,usuario) #Diccionario con vertices y sus distancias respecto del usuario
@@ -130,7 +148,7 @@ class Comandos(object):
 		print ("Iniciando impresion...")
 		tiempo_inicial = time()
 		lista_distancia = crear_lista_rw(orden) #Lo paso a la famosa lista de listas
-		for i in range(1,len(lista_distancia)):
+		for i in xrange(1,len(lista_distancia)):
 			if (lista_distancia[i] != []):
 				print ("Distancia ") + str(i) +  ": " + str(len(lista_distancia[i]))
 		tiempo_final = time()
@@ -155,9 +173,9 @@ class Comandos(object):
 		"""
 		comunidades = {}
 		
-		for i in range(ncaminos):
+		for i in xrange(ncaminos):
 			adyacentes = grafo.obtener_adyacentes(usuario)							
-			for j in range(lcamino):
+			for j in xrange(lcamino):
 				random = randint(0,len(adyacentes)-1)
 				nvertice = max_freq(grafo,adyacentes[random])
 				if nvertice not in comunidades:
